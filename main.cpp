fde4af51406ae7a1aa5d1ce17d35f1cf8915fb37
Marco177171 <msebastiani93@gmail.com>
2024-05-05 22:27:40 +0200

    vector creation update

--- main.cpp
+++ main.cpp
@@ -9,17 +9,18 @@ void debug(std::string message) {
 	std::cout << message << std::endl;
 }
 
-std::vector<std::pair<int, int>> parseCSVLine(const std::string& line) {
-	std::vector<std::pair<int, int>> keyValuePairs;
-	std::istringstream ss(line);
-	std::string csvLine;
-
-	while (std::getline(ss, csvLine)) {
-		std::size_t commaPos = csvLine.find(',');
-		if (commaPos != std::string::npos) {
-			int key = std::stoi(csvLine.substr(0, commaPos));
-			int value = std::stoi(csvLine.substr(commaPos + 1));
-			keyValuePairs.push_back({key, value});
+std::vector<std::pair<int, int>> *parseCSVLine(const std::string& line, std::vector<std::pair<int, int>> *keyValuePairs) {
+	std::size_t commaPos = line.find(',');
+	if (commaPos != std::string::npos) {
+        std::string keyStr = line.substr(0, commaPos);
+        std::string valueStr = line.substr(commaPos + 1);
+		try {
+			int key = std::stoi(line.substr(0, commaPos));
+			int value = std::stoi(line.substr(commaPos + 1));
+			keyValuePairs->push_back({key, value});
+		}
+		catch(const std::exception& e) {
+			std::cerr << e.what() << '\n';
 		}
 	}
 
@@ -40,14 +41,12 @@ int main(int argc, char *argv[]) {
 	}
 
 	std::string line;
-	while (std::getline(file, line)) {
-		std::vector<std::pair<int, int>> keyValuePairs = parseCSVLine(line);
-		// Process key-value pairs
-		for (const auto& pair : keyValuePairs) {
-			std::cout << "Key: " << pair.first << ", Value: " << pair.second << std::endl;
-		}
-	}
-
+	std::vector<std::pair<int, int>> keyValuePairs;
+	while (std::getline(file, line))
+		parseCSVLine(line, &keyValuePairs);
+	std::vector<std::pair<int, int>>::iterator it = keyValuePairs.begin();
+	while (it++ != keyValuePairs.end())
+		std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
 	file.close();
 
 	return 0;
